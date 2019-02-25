d088e106e878de525cb8399f9bc4d772fb8cc5c2
Arif Hanif <arif.hanif3@gmail.com>
2019-02-24 19:16:25 -0500

    add shell script to catch iddfactory error

---
+++ utils/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+make -j 2
+
+catchIddFactoryError() {
+  make GenerateIddFactory
+  make GenerateIddFactoryRun
+  make -j 2
+}
+
+trap "catchIddFactoryError" ERR
+
+return
+
+catchIddFactoryError
+
+cp ./Products/python/*.so ./python_wrapper/generated_sources
