3a33c6847811931edaefd40fd7b16e8978e5c63c
Arif Hanif <arif.hanif3@gmail.com>
2019-02-24 21:54:51 -0500

    fixed py3 imports

--- py3_openstudio_linux_sdk/__init__.py
+++ py3_openstudio_linux_sdk/__init__.py
@@ -1,30 +1,60 @@
 # import openstudioairflow as airflow
 
+
+
 from . import openstudioairflow as airflow
+
+
+
+
 
 
 
 # import openstudioenergyplus as energyplus
 
+
+
 from . import openstudioenergyplus as energyplus
+
+
+
+
 
 
 
 # import openstudiogbxml as gbxml
 
+
+
 from . import openstudiogbxml as gbxml
+
+
+
+
 
 
 
 # import openstudioisomodel as isomodel
 
+
+
 from . import openstudioisomodel as isomodel
+
+
+
+
 
 
 
 # import openstudiomeasure as measure
 
+
+
 from . import openstudiomeasure as measure
+
+
+
+
 
 
 
@@ -32,13 +62,27 @@ from . import openstudiomodel as model
 
 
 
+
+
+
+
 # import openstudiomodelcore as modelcore
+
+
+
+
 
 
 
 # import openstudiomodeleditor as modeleditor
 
+
+
 from . import openstudiomodeleditor as modeleditor
+
+
+
+
 
 
 
@@ -46,7 +90,15 @@ from . import openstudiomodeleditor as modeleditor
 
 
 
+
+
+
+
 # import openstudiomodelgeometry as modelgeometry
+
+
+
+
 
 
 
@@ -54,7 +106,15 @@ from . import openstudiomodeleditor as modeleditor
 
 
 
+
+
+
+
 # import openstudiomodelrefrigeration as modelrefrigeration
+
+
+
+
 
 
 
@@ -62,49 +122,99 @@ from . import openstudiomodeleditor as modeleditor
 
 
 
+
+
+
+
 # import openstudiomodelsimulation as modelsimulation
+
+
+
+
 
 
 
 # import openstudioosversion as osversion
 
+
+
 from .openstudioosversion import *
+
+
+
+
 
 
 
 # import openstudioradiance as radiance
 
+
+
 from . import openstudioradiance as radiance
+
+
+
+
 
 
 
 # import openstudiosdd as sdd
 
+
+
 from . import openstudiosdd as sdd
+
+
+
+
 
 
 
 # import openstudioutilities as utilities
 
+
+
 from .openstudioutilities import *
+
+
+
+
 
 
 
 # import openstudioutilitiesbcl as utilitiesbcl
 
+
+
 from .openstudioutilitiesbcl import *
+
+
+
+
 
 
 
 # import openstudioutilitiescore as utilitiescore
 
+
+
 from .openstudioutilitiescore import *
+
+
+
+
 
 
 
 # import openstudioutilitiesdata as utilitiesdata
 
+
+
 from .openstudioutilitiesdata import *
+
+
+
+
 
 
 
@@ -112,21 +222,43 @@ from .openstudioutilitiesdata import *
 
 
 
+
+
+
+
 # import openstudioutilitiesgeometry as utilitiesgeometry
+
+
 
 from .openstudioutilitiesgeometry import *
 
 
 
+
+
+
+
 # import openstudioutilitiesidd as utilitiesidd
+
+
 
 from .openstudioutilitiesidd import *
 
 
 
+
+
+
+
 # import openstudioutilitiesidf as utilitiesidf
 
+
+
 from .openstudioutilitiesidf import *
+
+
+
+
 
 
 
@@ -134,20 +266,41 @@ from .openstudioutilitiesidf import *
 
 
 
+
+
+
+
 # import openstudioutilitiessql as utilitiessql
 
+
+
 from .openstudioutilitiessql import *
+
+
 
 #from . import openstudioutilitiessql as sql
 
 
 
+
+
+
+
 # import openstudioutilitiestime as utilitiestime
+
+
 
 from .openstudioutilitiestime import *
 
 
 
+
+
+
+
 # import openstudioutilitiesunits as utilitiesunits
 
+
+
 from .openstudioutilitiesunits import *
+
