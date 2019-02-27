ed973ebac441e25f3bd116e093458ffff4444df5
Arif Hanif <arif.hanif3@gmail.com>
2019-02-26 21:53:31 -0500

    add __init__.py for bindings formatted for py3

---
+++ build/__init__.py
@@ -0,0 +1,20 @@
+from . import openstudioairflow as airflow
+from . import openstudioenergyplus as energyplus
+from . import openstudiogbxml as gbxml
+from . import openstudioisomodel as isomodel
+from . import openstudiomeasure as measure
+from . import openstudiomodel as model
+from . import openstudiomodeleditor as modeleditor
+from .openstudioosversion import *
+from . import openstudioradiance as radiance
+from . import openstudiosdd as sdd
+from .openstudioutilities import *
+from .openstudioutilitiesbcl import *
+from .openstudioutilitiescore import *
+from .openstudioutilitiesdata import *
+from .openstudioutilitiesgeometry import *
+from .openstudioutilitiesidd import *
+from .openstudioutilitiesidf import *
+from .openstudioutilitiessql import *
+from .openstudioutilitiestime import *
+from .openstudioutilitiesunits import *
